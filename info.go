6d649dc26fd669fb400760877483ef5c340e56e6
Leslie Dancsecs <lesdancsecs@gmail.com>
2025-07-18 15:36:44 -0300

    Removed redundant copyright message.

--- info.go
+++ info.go
@@ -25,19 +25,3 @@ errors for code tests.  Finally it provides for the capturing of logs and
 standard output streams with automatic diffs.
 */
 package sztest
-
-//   Golang testing utility.
-//   Copyright (C) 2023  Leslie Dancsecs
-//
-//   This program is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
